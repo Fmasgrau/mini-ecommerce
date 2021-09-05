44e3817ab042676e3e194c456ce29e67a8047143
Facundo Masgrau <facundomasgrau@gmail.com>
2021-09-05 03:18:33 +0000

    feat(localstorage): add localstorage to redux

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,12 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import ProductsReducer from '../features/products/reducer/productsReducer'
 import CartReducer from '../features/products/reducer/cartReducer'
+import { saveState, loadState } from '../utils/localStorage'
+
+const initialData: any = loadState()
 
 const store = configureStore({
+  preloadedState: initialData,
   reducer: {
     products: ProductsReducer,
     cart: CartReducer
-  }
+  },
+
+})
+
+store.subscribe(() => {
+  saveState(store.getState())
 })
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
---
+++ src/utils/localStorage.ts
@@ -0,0 +1,23 @@
+
+export const loadState = () => {
+    try {
+        const serializedData = localStorage.getItem('state')
+        if (serializedData === null) {
+
+            return undefined
+        }
+        console.log(JSON.parse(serializedData))
+        return JSON.parse(serializedData)
+    } catch (error) {
+        console.log("Error to load state from LocalStorage")
+        return undefined
+    }
+}
+export const saveState = (state: any) => {
+    try {
+        const serializedData = JSON.stringify(state)
+        localStorage.setItem('state', serializedData)
+    } catch (error) {
+        console.log("error to save state in LocalStorage")
+    }
+}
